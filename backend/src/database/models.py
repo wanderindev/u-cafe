5d3ef2fa0df1093f108e9040a36f805a298a3ec9
Javier Feliu <javier@wanderin.dev>
2019-12-27 22:38:53 -0500

    refactor: Clean up code in models.py

--- backend/src/database/models.py
+++ backend/src/database/models.py
@@ -1,5 +1,6 @@
 import os
-from sqlalchemy import Column, String, Integer
+import sys
+from sqlalchemy import Column, exc, Integer, String
 from flask_sqlalchemy import SQLAlchemy
 import json
 
@@ -11,53 +12,32 @@ database_path = "sqlite:///{}".format(
 
 db = SQLAlchemy()
 
-"""
-setup_db(app)
-    binds a flask application and a SQLAlchemy service
-"""
-
 
 def setup_db(app):
+    """Bind a flask application and a SQLAlchemy service."""
     app.config["SQLALCHEMY_DATABASE_URI"] = database_path
     app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
     db.app = app
     db.init_app(app)
 
 
-"""
-db_drop_and_create_all()
-    drops the database tables and starts fresh
-    can be used to initialize a clean database
-    !!NOTE you can change the database_filename variable to have multiple verisons of a database
-"""
-
-
 def db_drop_and_create_all():
+    """Drop the database tables and start fresh."""
     db.drop_all()
     db.create_all()
 
 
-"""
-Drink
-a persistent drink entity, extends the base SQLAlchemy Model
-"""
-
-
 class Drink(db.Model):
-    # Autoincrementing, unique primary key
+    """A persistent drink entity, extends the base SQLAlchemy Model"""
+
     id = Column(Integer().with_variant(Integer, "sqlite"), primary_key=True)
-    # String Title
     title = Column(String(80), unique=True)
-    # the ingredients blob - this stores a lazy json blob
-    # the required datatype is [{'color': string, 'name':string, 'parts':number}]
+    # A recipe is a json string with the following structure
+    # [{'color': string, 'name':string, 'parts':number}]
     recipe = Column(String(180), nullable=False)
 
-    """
-    short()
-        short form representation of the Drink model
-    """
-
     def short(self):
+        """Short form representation of the Drink model."""
         print(json.loads(self.recipe))
         short_recipe = [
             {"color": r["color"], "parts": r["parts"]}
@@ -65,57 +45,52 @@ class Drink(db.Model):
         ]
         return {"id": self.id, "title": self.title, "recipe": short_recipe}
 
-    """
-    long()
-        long form representation of the Drink model
-    """
-
     def long(self):
+        """Long form representation of the Drink model."""
         return {
             "id": self.id,
             "title": self.title,
             "recipe": json.loads(self.recipe),
         }
 
-    """
-    insert()
-        inserts a new model into a database
-        the model must have a unique name
-        the model must have a unique id or null id
-        EXAMPLE
-            drink = Drink(title=req_title, recipe=req_recipe)
-            drink.insert()
-    """
-
     def insert(self):
-        db.session.add(self)
-        db.session.commit()
-
-    """
-    delete()
-        deletes a new model into a database
-        the model must exist in the database
-        EXAMPLE
-            drink = Drink(title=req_title, recipe=req_recipe)
-            drink.delete()
-    """
+        """Insert a new recipe into a database."""
+        try:
+            db.session.add(self)
+            db.session.commit()
+        except exc.SQLAlchemyError as e:
+            print(e)
+            print(sys.exc_info())
+            db.session.rollback()
+            return {"error": True}
+        finally:
+            db.session.close()
 
     def delete(self):
-        db.session.delete(self)
-        db.session.commit()
+        """Delete a recipe from the database"""
+        try:
+            db.session.delete(self)
+            db.session.commit()
+        except exc.SQLAlchemyError as e:
+            print(e)
+            print(sys.exc_info())
+            db.session.rollback()
+            return {"error": True}
+        finally:
+            db.session.close()
 
-    """
-    update()
-        updates a new model into a database
-        the model must exist in the database
-        EXAMPLE
-            drink = Drink.query.filter(Drink.id == id).one_or_none()
-            drink.title = 'Black Coffee'
-            drink.update()
-    """
-
-    def update(self):
-        db.session.commit()
+    @staticmethod
+    def update():
+        """Update a recipe in the database"""
+        try:
+            db.session.commit()
+        except exc.SQLAlchemyError as e:
+            print(e)
+            print(sys.exc_info())
+            db.session.rollback()
+            return {"error": True}
+        finally:
+            db.session.close()
 
     def __repr__(self):
         return json.dumps(self.short())
