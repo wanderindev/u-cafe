303be88c15a11e212c94600461fdcc505f016112
Javier Feliu <javier@wanderin.dev>
2020-01-06 22:52:36 -0500

    feat: Add run.py

--- backend/src/api.py
+++ backend/src/api.py
@@ -2,7 +2,7 @@ from flask import Flask, request, jsonify, abort
 import json
 from flask_cors import CORS
 
-from database.models import db_drop_and_create_all, setup_db, Drink
+from .database.models import db_drop_and_create_all, setup_db, Drink
 from auth.auth import AuthError, requires_auth
 
 app = Flask(__name__)
@@ -168,7 +168,3 @@ def auth_error(error):
         ),
         error.status_code,
     )
-
-
-if __name__ == "__main__":
-    app.run(debug=True)
