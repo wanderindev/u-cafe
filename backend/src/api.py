8189a820ef95943c08ff79a72f282abb43864772
Javier Feliu <javier@wanderin.dev>
2020-01-09 22:00:53 -0500

    feat: Add authentication to Postman collection

--- backend/src/api.py
+++ backend/src/api.py
@@ -2,7 +2,7 @@ from flask import Flask, request, jsonify, abort
 import json
 from flask_cors import CORS
 
-from .database.models import db_drop_and_create_all, setup_db, Drink
+from database.models import db_drop_and_create_all, setup_db, Drink
 from auth.auth import AuthError, requires_auth
 
 app = Flask(__name__)
